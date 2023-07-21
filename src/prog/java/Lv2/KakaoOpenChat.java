4e748494a5680810a42f4d740af27056e9303f7c
crushon9 <you@example.com>
2023-07-21 13:57:06 +0900

    complete KakaoOpenChat

--- src/prog/java/Lv2/KakaoOpenChat.java
+++ src/prog/java/Lv2/KakaoOpenChat.java
@@ -9,29 +9,36 @@ public class KakaoOpenChat {
         // 유저 아이디에 매칭되는 최종 별명 hashMap에 담기
         for (int i = 0; i < record.length; i++) {
             String[] rcd = record[i].split(" ");
-            if (rcd[0].equals("Enter") || rcd[0].equals("Change")) {
+            if (rcd[0].equals("Enter")) {
                 // put : 이미 키값이 있을 경우 최신값으로 덮어씀
                 userInfo.put(rcd[1], rcd[2]);
                 ansLn++;
+            } else if (rcd[0].equals("Change")) {
+                userInfo.put(rcd[1], rcd[2]);
+            } else {
+                ansLn++;
             }
         }
-        // Enter와 Change 일때만 메세지를 출력하므로 answer길이 초기화
+        // Enter와 Leave 일때만 메세지를 출력하므로 answer길이 초기화
         String[] answer = new String[ansLn];
 
+        int cnt = ansLn;
         // answer 에 메세지 넣기
         for (int i = 0; i < record.length; i++) {
             String[] rcd = record[i].split(" ");
             if (rcd[0].equals("Enter")) {
-                answer[i] = userInfo.get(rcd[1]) + "님이 들어왔습니다.";
+                answer[ansLn - cnt] = userInfo.get(rcd[1]) + "님이 들어왔습니다.";
+                cnt--;
             } else if (rcd[0].equals("Leave")) {
-                answer[i] = userInfo.get(rcd[1]) + "님이 나갔습니다.";
+                answer[ansLn - cnt] = userInfo.get(rcd[1]) + "님이 나갔습니다.";
+                cnt--;
             }
         }
         return answer;
     }
 
     public static void main(String[] args) {
-        String[] record = {"Enter uid1234 Muzi", "Enter uid4567 Prodo","Leave uid1234","Enter uid1234 Prodo","Change uid4567 Ryan"};
+        String[] record = {"Enter uid1234 Muzi", "Enter uid4567 Prodo", "Leave uid1234", "Enter uid1234 Prodo", "Change uid4567 Ryan"};
         for (String x : solution(record)) {
             System.out.println(x);
         }
