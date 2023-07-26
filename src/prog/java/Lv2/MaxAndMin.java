c3ee7b55c90794b8a6c4ed6d9c41997bd28d1643
crushon9 <catco929@naver.com>
2023-07-26 17:32:41 +0900

    update best practice

--- src/prog/java/Lv2/MaxAndMin.java
+++ src/prog/java/Lv2/MaxAndMin.java
@@ -17,3 +17,18 @@ class MaxAndMin {
         return answer;
     }
 }
+
+/*
+    public String getMinMaxString(String str) {
+        String[] tmp = str.split(" ");
+        int min, max, n;
+        min = max = Integer.parseInt(tmp[0]);
+        for (int i = 1; i < tmp.length; i++) {
+            n = Integer.parseInt(tmp[i]);
+            if(min > n) min = n;
+            if(max < n) max = n;
+        }
+
+        return min + " " + max;
+
+    }*/
