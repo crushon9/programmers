8d57a9133afd5ac4f862fc7458ccff76f080ac54
crushon9 <catco929@naver.com>
2023-07-28 13:55:25 +0900

    solve new lesson

---
+++ src/prog/java/Lv2/Fibonacci.java
@@ -0,0 +1,26 @@
+package prog.java.Lv2;
+
+/* 2 이상의 n이 입력되었을 때, n번째 피보나치 수를 1234567으로 나눈 나머지를 리턴하는 함수, solution을 완성해 주세요. */
+
+public class Fibonacci {
+    public static int solution(int n) {
+
+        int ppre = 0;
+        int pre = 1;
+        int fibo = 0;
+        for (int i = 1; i < n; i++) {
+            ppre = ppre % 1234567;
+            pre = pre % 1234567;
+            fibo = ppre + pre;
+            ppre = pre;
+            pre = fibo;
+        }
+
+        return fibo % 1234567;
+    }
+
+    public static void main(String[] args) {
+
+        System.out.println(solution(7));
+    }
+}
