164039b9a4a19ac1cb20ac0a14038cf199b0b449
crushon9 <catco929@naver.com>
2023-07-28 13:13:52 +0900

    solve new lesson

---
+++ src/prog/java/Lv2/BinaryMin.java
@@ -0,0 +1,70 @@
+package prog.java.Lv2;
+
+public class BinaryMin {
+    public static int solution(int n) {
+        int answer = 0;
+
+        int lenOfN1 = Integer.bitCount(n);
+        int cnt = 1;
+        boolean flag = true;
+        while (flag) {
+            if (lenOfN1 == Integer.bitCount(n + cnt)) {
+                answer = n + cnt;
+                flag = false;
+            } else {
+                cnt++;
+            }
+        }
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(solution(15));
+    }
+}
+
+/*
+public class BinaryMin {
+    public static int solution(int n) {
+        int answer = 0;
+
+        int lenOfN1 = removeZero(biCasting(n, ""));
+        int cnt = 1;
+        boolean flag = true;
+        while (flag) {
+            if (lenOfN1 == removeZero(biCasting(n + cnt, ""))) {
+                answer = n + cnt;
+                flag = false;
+            } else {
+                cnt++;
+            }
+        }
+
+        return answer;
+    }
+
+    public static int removeZero(String s1) {
+        int len = s1.length();
+        int cnt0 = 0;
+        for (int i = 0; i < len; i++) {
+            if (s1.charAt(i) == '0') {
+                cnt0++;
+            }
+        }
+        return len - cnt0;
+    }
+
+    public static String biCasting(int num, String str) {
+        str = (num % 2) + str;
+        if (num / 2 > 0) {
+            return biCasting(num / 2, str);
+        } else {
+            return str;
+        }
+    }
+
+    public static void main(String[] args) {
+        System.out.println(solution(15));
+    }
+}*/
