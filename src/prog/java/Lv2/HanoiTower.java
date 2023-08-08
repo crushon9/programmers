50f81ff9ff944641c71dfc68bbd42f2b3bf922c8
crushon9 <catco929@naver.com>
2023-08-08 15:55:28 +0900

    hanoi figure out

--- src/prog/java/Lv2/HanoiTower.java
+++ src/prog/java/Lv2/HanoiTower.java
@@ -20,14 +20,14 @@ public class HanoiTower {
     }
 
     public void hanoi(int n, int from, int temp, int to) {
-        if (n == 1) { // 원반이 하나만 있을때는 (재귀 탈출 조건)
-            int[] arr = {from, to}; // 바로 목표지점으로 옮김
+        if (n == 1) { // 원반이 하나만 있을 때는 (재귀 탈출 조건)
+            int[] arr = {from, to}; // 그냥 목표지점으로 옮김
             list.add(arr);
-        } else { // 원반이 적어도 두개 이상일때는 (재귀 호출시 규모를 줄여가며 호출)
-            hanoi(n - 1, from, to, temp); // 제일아래원반을제외한 위쪽원반들은 임시막대로 옮기고
-            int[] arr = {from, to}; // 제일아래원반을 목표지점으로 옮기고
+        } else { // 원반이 두개 이상일 때는 (재귀 호출시 n-1처럼 규모를 줄여 호출)
+            hanoi(n - 1, from, to, temp); // 제일밑원반을 제외한 위쪽원반들은 임시막대로 옮기고(재귀적으로)
+            int[] arr = {from, to}; // 제일밑원반을 목표지점으로 옮기고
             list.add(arr);
-            hanoi(n - 1, temp, from, to); // 위쪽원반들을 임시막대에서 최종목표로 옮김
+            hanoi(n - 1, temp, from, to); // 위쪽원반들을 임시막대에서 최종목표로 옮김(재귀적으로)
         }
     }
 
